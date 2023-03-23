245a807a6be880351768ef3b88eb2c0eb1e79795
jinwoo-Bae <wlsdn3578@gmail.com>
2023-03-23 22:30:23 +0900

    feat: Add swagger to project

---
+++ src/main/java/com/devJeans/rabbit/bind/ApiResult.java
@@ -0,0 +1,2 @@
+package com.devJeans.rabbit.bind;public class ApiResult {
+}
--- src/main/java/com/devJeans/rabbit/config/SecurityConfig.java
+++ src/main/java/com/devJeans/rabbit/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.devJeans.rabbit.config;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +13,21 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String[] PERMIT_URL_ARRAY = {
+            /* swagger v2 */
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            /* swagger v3 */
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
+
+
     private final JWTRequestFilter jwtRequestFilter;
 
     public SecurityConfig(JWTRequestFilter jwtRequestFilter) {
@@ -20,14 +36,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
-        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .ignoringAntMatchers("/v1/oauth/login");
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-        http.authorizeRequests()
+        http
+                .csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/v1/oauth/login").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers(PERMIT_URL_ARRAY).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
 
