20557c52ed7670014ebbe4ae7934e3052106ecda
jinwoo-Bae <wlsdn3578@gmail.com>
2023-04-17 21:58:09 +0900

    feat: Add the function to hide inappropriate photo

--- src/main/java/com/devJeans/rabbit/repository/PhotoRepository.java
+++ src/main/java/com/devJeans/rabbit/repository/PhotoRepository.java
@@ -1,8 +1,11 @@
 package com.devJeans.rabbit.repository;
 
 import com.devJeans.rabbit.domain.Photo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.LockModeType;
@@ -14,4 +17,8 @@ public interface PhotoRepository extends JpaRepository<Photo, Long> {
     Photo save(Photo photo);
     Optional<Photo> findById(Long photoId);
     Optional<Photo> findByImageKeyName(String iamgeKeyName);
+
+    @Query("SELECT p FROM Photo p WHERE p.isShow = true")
+    Page<Photo> findPhotosWhereIsShowTrue(Pageable pageable);
+
 }
